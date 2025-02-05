cdce0a24c98b53055fb4239ee1e8efb7f8f9b5e1
ATHIRA UNNIKRISHNAN <164757424+Athira-unni13@users.noreply.github.com>
2025-02-05 22:13:32 +0530

    Add files via upload

---
+++ regexpack/RegexExample.java
@@ -0,0 +1,11 @@
+package regexpack;
+
+import java.util.regex.Pattern;
+
+public class RegexExample {
+
+	public static void main(String[] args) {
+		Pattern p = Pattern.compile(".s");//Represent single character
+	}
+
+}
